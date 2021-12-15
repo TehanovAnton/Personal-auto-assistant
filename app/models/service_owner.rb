7ea1b48ebe4d8cdcba5775bc7212c578ecff2396
anton <tehanovanton@gmail.com>
2021-12-15 19:10:48 +0300

    Add ServiceOwner default scope

--- app/models/service_owner.rb
+++ app/models/service_owner.rb
@@ -1,17 +1,3 @@
 class ServiceOwner < User
-  after_initialize :default_role
-
-  def self.all
-    User.select { |user| user.role == service_owner_role }
-  end
-
-  private
-
-  def default_role
-    self.role = service_owner_role
-  end
-
-  def service_owner_role
-    @default_role ||= 'service_owner'
-  end
+  default_scope { where(role: :service_owner) }
 end
