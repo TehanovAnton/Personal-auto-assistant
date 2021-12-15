e98fa9c04c5d79b33aeda2c8f941f50aa97582cf
anton <tehanovanton@gmail.com>
2021-12-15 20:24:12 +0300

    Add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   enum role: { car_owner: 0,
                service_owner: 1,
                admin: 2 }
+
+  validates :first_name, :last_name, :email, :phone_number, presence: true
+  validates :first_name, :last_name, length: { in: 2..30 }
+  validates :role, inclusion: { in: %w[car_owner service_owner admin] }
 end
