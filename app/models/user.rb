7944aa2fb9f4cfbb8528ad536af8b7662492d2e0
anton <tehanovanton@gmail.com>
2021-12-16 19:11:21 +0300

    Generate devse for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   enum role: { car_owner: 0,
                service_owner: 1,
                admin: 2 }
--- db/migrate/20211211191649_create_users.rb
+++ db/migrate/20211211191649_create_users.rb
@@ -6,12 +6,12 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :phone_number, null: false
-      t.string :email, null: false
+      # t.string :email, null: false
       t.integer :role, null: false, default: 0
 
       t.timestamps
     end
     add_index :users, :phone_number, unique: true
-    add_index :users, :email, unique: true
+    # add_index :users, :email, unique: true
   end
 end
