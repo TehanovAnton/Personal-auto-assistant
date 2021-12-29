7d64b73c06d283d34d70678e6f324ce4f24dc725
anton <tehanovanton@gmail.com>
2021-12-29 19:25:39 +0300

    Set associations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ApplicationRecord
+  has_many :car_consumable_values, dependent: :destroy
+  has_many :consumables, through: :car_consumable_values
 end
--- app/models/consumable.rb
+++ app/models/consumable.rb
@@ -1,2 +1,4 @@
 class Consumable < ApplicationRecord
+  has_many :car_consumable_values
+  has_many :cars, through: :car_consumable_values
 end
