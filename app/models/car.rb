50e42418680a49926ff4d383151341e765b4b839
anton <tehanovanton@gmail.com>
2021-12-31 13:05:38 +0300

    Add associations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ApplicationRecord
+  has_many :car_parts, dependent: :destroy
+  has_many :parts, through: :car_parts
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,4 @@
 class Part < ApplicationRecord
+  has_many :car_parts, dependent: :destroy
+  has_many :cars, through: :car_parts
 end
