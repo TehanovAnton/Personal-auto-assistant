c7d5ffbc2341c9c0660cfc26c69bf49d5574a60a
anton <tehanovanton@gmail.com>
2022-01-02 20:21:11 +0300

    Implement document name using enum

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,9 @@
 class Document < ApplicationRecord
+  enum name: {
+    license: 0,
+    insurence: 1
+  }
+
   has_many :car_owner_documents, dependent: :destroy
   has_many :users, through: :car_owner_documents
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_162859) do
+ActiveRecord::Schema.define(version: 2022_01_02_165001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,10 +61,9 @@ ActiveRecord::Schema.define(version: 2022_01_02_162859) do
   end
 
   create_table "documents", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_documents_on_name", unique: true
+    t.integer "name", default: 0, null: false
   end
 
   create_table "guests", force: :cascade do |t|
