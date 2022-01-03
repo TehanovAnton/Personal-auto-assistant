5b5ecf7ced5a70c1861a87f6753d9c6c197ca899
anton <tehanovanton@gmail.com>
2022-01-03 20:24:53 +0300

    Add conusmable validation

--- app/models/car_consumable_value.rb
+++ app/models/car_consumable_value.rb
@@ -1,4 +1,6 @@
 class CarConsumableValue < ApplicationRecord
   belongs_to :car, dependent: :destroy
   belongs_to :consumable, dependent: :destroy
+
+  validates :value, numericality: { greater_than: 0 }
 end
--- app/models/consumable.rb
+++ app/models/consumable.rb
@@ -1,4 +1,6 @@
 class Consumable < ApplicationRecord
-  has_many :car_consumable_values
+  has_many :car_consumable_values, dependent: :destroy
   has_many :cars, through: :car_consumable_values
+
+  validates :name, presence: true
 end
