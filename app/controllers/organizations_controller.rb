d72896a72e2264a3c2914b2291b718d6341edab3
anton <tehanovanton@gmail.com>
2022-01-03 17:19:37 +0300

    Rewrite OrganizationService using Rails build in methods

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,55 +1,50 @@
 class OrganizationsController < ApplicationController
-  before_action :set_organization, only: %i[show edit update destroy]
-  before_action :set_cities, only: %i[new create edit update]
+  before_action :set_organization, only: %i[show edit destroy]
+  before_action :set_cities, only: %i[new edit]
+
+  before_action -> { organization(organization_id: params[:id]) },
+                :organization_service,
+                only: %i[create update]
+
+  attr_accessor :service
 
   def index
     @organizations = Organization.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @organization = Organization.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
-    @organization = Organization.new(organization_params)
-    saved = service.save_organization_and_cities_organization(organization: @organization,
-                                                               city_ids: city_ids)
-
-    redirect_to organization_path(@organization), notice: 'Organization was successfully created.' if saved
-    render :new, status: :unprocessable_entity unless saved
+    if service.save_organization_and_cities_organization
+      redirect_to @organization, notice: 'Organization was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
-    respond_to do |format|
-      if @organization.update(organization_params)
-        format.html { redirect_to @organization, notice: "Organization was successfully updated." }
-        format.json { render :show, status: :ok, location: @organization }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
-      end
+    if service.update_organization_and_cities_organization(organization_params)
+      redirect_to @organization, notice: 'Organization was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @organization.destroy
-    respond_to do |format|
-      format.html { redirect_to organizations_url, notice: "Organization was successfully destroyed." }
-      format.json { head :no_content }
+    if @organization.destroy
+      redirect_to organizations_url, notice: 'Organization was successfully destroyed.'
+    else
+      render :destroy, status: :unprocessable_entity
     end
   end
 
   private
-
-  def service
-    @service ||= OrganizationService.new
-  end
 
   def set_organization
     @organization = Organization.find(params[:id])
@@ -59,12 +54,20 @@ class OrganizationsController < ApplicationController
     @cities = City.all
   end
 
+  def organization(organization_id: nil)
+    @organization = organization_id ? Organization.find_by(id: organization_id) : Organization.new(organization_params)
+  end
+
+  def organization_service
+    @service = OrganizationService.new(organization: @organization, city_ids: city_ids)
+  end
+
   def organization_params
     params.require(:organization).permit(:email, :phone_number, :adress, :name)
   end
 
   def city_ids
-    params.require(:organization).permit(city_ids: [])
+    city_ids = params.require(:organization).permit(city_ids: [])[:city_ids]
+    city_ids.select { |id| id unless id.empty? }.map(&:to_i)
   end
-    
 end
--- app/services/organization_service.rb
+++ app/services/organization_service.rb
@@ -1,12 +1,26 @@
 class OrganizationService
-  def save_organization_and_cities_organization(organization: Organization.new,
-                                                 city_ids: [])
-    return false unless organization.valid?
+  attr_accessor :organization, :cities
 
-    organization.save
+  def initialize(organization:, city_ids:)
+    @organization = organization
+    @cities = city_ids.map { |city_id| City.find_by(id: city_id) }
+  end
 
-    city_ids.each do |city_id|
-      CitiesOrganization.create(city_id: city_id, organization_id: organization.id)
-    end
+  def save_organization_and_cities_organization
+    return false unless organization.save
+
+    organization_cities
+  end
+
+  def update_organization_and_cities_organization(params)
+    return false unless organization.update(params)
+
+    organization_cities
+  end
+
+  private
+
+  def organization_cities
+    organization.cities = cities
   end
 end
