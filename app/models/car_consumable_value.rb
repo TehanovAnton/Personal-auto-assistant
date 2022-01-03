f8820c456f013e158ea19e1bbff26308395e4598
anton <tehanovanton@gmail.com>
2022-01-03 21:09:29 +0300

    Add factories
    Add consumable, user_with_car factories

--- app/models/car_consumable_value.rb
+++ app/models/car_consumable_value.rb
@@ -2,5 +2,5 @@ class CarConsumableValue < ApplicationRecord
   belongs_to :car, dependent: :destroy
   belongs_to :consumable, dependent: :destroy
 
-  validates :value, numericality: { greater_than: 0 }
+  validates :value, numericality: { greater_than_or_equal_to: 0 }
 end
--- factories/factories.rb
+++ factories/factories.rb
@@ -1,4 +1,22 @@
 FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { Faker::PhoneNumber.cell_phone  }
+    email { "#{first_name}#{last_name}@gmail.com" }
+    password { 'ewqqwe' }
+
+    factory :user_with_car do
+      after(:create) do |user|
+        create_list(:car, 1, user_id: user.id)
+
+        user.reload
+      end
+    end
+
+    after(:create, &:confirm)
+  end
+
   factory :car do
     user_id { User.last.id }
     model { Faker::Vehicle.model }
@@ -10,21 +28,15 @@ FactoryBot.define do
     transmission_type { %i[automatic mechanic].sample }
     maker { Faker::Vehicle.make }
     vin { Faker::Vehicle.vin }
+
+    after(:create) do |car|
+      create(:consumable)
+      car.consumables = Consumable.all
+      car.reload
+    end
   end
 
-  trait :with_user do
-    user_id { FactoryBot.create(:user).id }
+  factory :consumable do
+    name { 'fuel' }
   end
-end
-
-FactoryBot.define do
-  factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    phone_number { Faker::PhoneNumber.cell_phone  }
-    email { "#{first_name}#{last_name}@gmail.com" }
-    password { 'ewqqwe' }
-  end
-
-  after(:create) { |user| user.confirm if user.is_a? User }
 end
