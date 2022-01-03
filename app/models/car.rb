26dbbb47ee323c4f2b4747ab605cda2780a50a8e
anton <tehanovanton@gmail.com>
2022-01-03 22:35:46 +0300

    Add car validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,4 +6,14 @@ class Car < ApplicationRecord
 
   enum fuel_type: { electricity: 0, gas: 1, petrol: 2 }
   enum transmission_type: { mechanic: 0, automatic: 1 }
+
+  validates :model, :year_production, :engine_volume,
+            :mileage, :body_type, :fuel_type,
+            :transmission_type, :maker, :vin,
+            presence: true
+
+  validates :vin, uniqueness: true
+  validates :engine_volume, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3.5 }
+  validates :fuel_type, inclusion: { in: Car.fuel_types.keys }
+  validates :transmission_type, inclusion: { in: Car.transmission_types.keys }
 end
