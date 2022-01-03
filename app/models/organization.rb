6b404f0ba911359e84131c5557e5efc6481de14a
anton <tehanovanton@gmail.com>
2022-01-03 13:59:21 +0300

    Organization validation in one line

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,5 @@ class Organization < ApplicationRecord
   has_many :cities_organizations, dependent: :destroy
   has_many :cities, through: :cities_organizations
 
-  validates :adress, :email, :name, :phone_number, presence: true
-  validates :adress, :email, :name, :phone_number, uniqueness: true
+  validates :adress, :email, :name, :phone_number, presence: true, uniqueness: true
 end
