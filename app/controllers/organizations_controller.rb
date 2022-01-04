f709232fc34fca6fce3c7f3f88cbf24a6643d8e0
anton <tehanovanton@gmail.com>
2022-01-04 12:10:54 +0300

    Rewrite organizations_controller without organization_service

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,10 +3,7 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: %i[show edit destroy]
   before_action :set_cities, only: %i[new edit]
-
-  before_action -> { organization(organization_id: params[:id]) },
-                :organization_service,
-                only: %i[create update]
+  before_action -> { organization(organization_id: params[:id]) }, only: %i[create update]
 
   attr_accessor :service
 
@@ -23,7 +20,7 @@ class OrganizationsController < ApplicationController
   def edit; end
 
   def create
-    if service.save_organization_and_cities_organization
+    if @organization.save
       redirect_to @organization, notice: 'Organization was successfully created.'
     else
       render :new, status: :unprocessable_entity
@@ -31,7 +28,7 @@ class OrganizationsController < ApplicationController
   end
 
   def update
-    if service.update_organization_and_cities_organization(organization_params)
+    if @organization.update(organization_params)
       redirect_to @organization, notice: 'Organization was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
@@ -39,7 +36,7 @@ class OrganizationsController < ApplicationController
   end
 
   def destroy
-    if @organization.destroy
+    if @organization.delete
       redirect_to organizations_url, notice: 'Organization was successfully destroyed.'
     else
       render :destroy, status: :unprocessable_entity
@@ -60,16 +57,7 @@ class OrganizationsController < ApplicationController
     @organization = organization_id ? Organization.find_by(id: organization_id) : Organization.new(organization_params)
   end
 
-  def organization_service
-    @service = OrganizationService.new(organization: @organization, city_ids: city_ids)
-  end
-
   def organization_params
-    params.require(:organization).permit(:email, :phone_number, :adress, :name)
-  end
-
-  def city_ids
-    city_ids = params.require(:organization).permit(city_ids: [])[:city_ids]
-    city_ids.select { |id| id unless id.empty? }.map(&:to_i)
+    params.require(:organization).permit(:email, :phone_number, :adress, :name, city_ids: [])
   end
 end
