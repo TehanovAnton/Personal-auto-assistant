e1387df442c4694f4da0db67698961f0876def7f
anton <tehanovanton@gmail.com>
2022-01-04 12:43:05 +0300

    Add document validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ApplicationRecord
   has_many :car_owner_documents, dependent: :destroy
   has_many :users, through: :car_owner_documents
+
+  validates :name, presence: true
+  validates :term_of_validity, numericity: { only_integer: true, greater_than: 28 }
 end
