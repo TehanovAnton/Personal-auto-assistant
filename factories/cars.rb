273af9d70d046125671818b06dadf3039e953968
anton <tehanovanton@gmail.com>
2022-01-04 13:18:37 +0300

    Split fcatories by files

--- factories/cars.rb
+++ factories/cars.rb
@@ -1,22 +1,6 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    phone_number { Faker::PhoneNumber.cell_phone  }
-    email { "#{first_name}#{last_name}@gmail.com" }
-    password { 'ewqqwe' }
-
-    factory :user_with_car do
-      after(:create) do |user|
-        create_list(:car, 1, user_id: user.id)
-
-        user.reload
-      end
-    end
-
-    after(:create, &:confirm)
-  end
-
   factory :car do
     user_id { User.last.id }
     model { Faker::Vehicle.model }
@@ -34,9 +18,5 @@ FactoryBot.define do
       car.consumables = Consumable.all
       car.reload
     end
-  end
-
-  factory :consumable do
-    name { 'fuel' }
   end
 end
