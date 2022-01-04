ae3875bdce667ec121937c72f0c2b1938d2d7534
anton <tehanovanton@gmail.com>
2022-01-04 17:58:21 +0300

    Add issue_date validation

--- app/models/cars_owners_document.rb
+++ app/models/cars_owners_document.rb
@@ -1,4 +1,6 @@
 class CarsOwnersDocument < ApplicationRecord
   belongs_to :user
   belongs_to :document
+
+  validates :issue_date, presence: true, if: -> { issue_date > Time.current.to_date }
 end
