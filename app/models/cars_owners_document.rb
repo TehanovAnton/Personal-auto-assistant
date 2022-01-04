8641a3e082de916f9e12e64a7b25ab47abec37c0
anton <tehanovanton@gmail.com>
2022-01-04 20:45:23 +0300

    Add issue_date validation

--- app/models/cars_owners_document.rb
+++ app/models/cars_owners_document.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 class CarsOwnersDocument < ApplicationRecord
   belongs_to :user
   belongs_to :document
 
-  validates :issue_date, presence: true, if: -> { issue_date > Time.current.to_date }
+  validates :issue_date, presence: true
+  validate :earlier_than_now?
+
+  private
+
+  def earlier_than_now?
+    errors.add(:issue_date, 'Document issue date nust be earlier than now') unless issue_date < Time.current.to_date
+  end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Document < ApplicationRecord
   has_many :car_owner_documents, dependent: :destroy
   has_many :users, through: :car_owner_documents
