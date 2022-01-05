d182a33c82b872a5809fe0bda549f5d11a14d2c9
anton <tehanovanton@gmail.com>
2022-01-05 17:39:18 +0300

    Disable access for a not signed in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :sign_in_unless_confirmed, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone_number role])
+  end
+
+  def sign_in_unless_confirmed
+    message = "Your email isn't confirmed"
+    redirect_to new_user_session_path, alert: message unless user_confirmed?
+  end
+
+  def user_confirmed?
+    return false unless current_user
+
+    current_user.confirmed?
   end
 end
