f7e8a542c0a6bc7bb869b64b1273dbeb04fe203b
Anton <anton@wse-032-71b.icx.local>
2022-03-21 17:32:47 +0300

    Add admin user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 %w[Minsk Brest Mogilev].each { |name| City.create(name: name) }
 
 # Car wil have consumables, parts. User will have documents
@@ -18,4 +10,4 @@ Category.create(name: 'cleaning')
 
 FactoryBot.create(:organization_with_service, name: 'apolo')
 
-# FactoryBot.create(:order, car_id: Car.last.id, services_work_id: ServicesWork.last.id, mileage: Car.last.mileage)
+FactoryBot.create(:user, role: :admin)
