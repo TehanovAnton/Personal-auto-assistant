a1f8a5c73f1164276ef07a4db47257255cceec5b
anton <tehaonvanton@gmail.com>
2022-05-19 19:25:51 +0300

    Car graphql type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,16 +3,20 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
     field :user, Types::UserType, null: false, description: "user by id" do
+      argument :id, ID
+    end
+
+    field :car, Types::CarType, null: false, description: "car by id" do
       argument :id, ID
     end
 
     def user(id:)
       User.find(id)
+    end
+
+    def car(id:)
+      Car.find(id)
     end
   end
 end
