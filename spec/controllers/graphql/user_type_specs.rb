bb0a8f4cbf343c69aecac60b1633cf441590625c
anton <tehanovanton@gmail.com>
2022-05-27 15:16:02 +0300

    Graphql user type negative test

--- spec/controllers/graphql/user_type_specs.rb
+++ spec/controllers/graphql/user_type_specs.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
   let(:user) { create(:user, role: :car_owner) }
+  before { sign_in user }
 
   describe '#users' do
     context 'when correct request' do
@@ -19,8 +20,6 @@ RSpec.describe GraphqlController, type: :controller do
         GQL
       end
 
-      before { sign_in user }
-
       it 'should return array of users' do
         result = PersonalAutoAssitatntSchema.execute(query)
         expect(result['data']['users']).not_to be_empty
@@ -31,6 +30,21 @@ RSpec.describe GraphqlController, type: :controller do
         expect(result['data']['users'].first['firstName']).to be
         expect(result['data']['users'].first['lastName']).to be
         expect(result['data']['users'].first['email']).to be
+      end
+    end
+
+    context 'when incorrect request' do
+      let(:query) do
+        <<~GQL
+          query {#{' '}
+            users { unexistedField }#{' '}
+          }
+        GQL
+      end
+
+      it 'should return response with errors' do
+        response = PersonalAutoAssitatntSchema.execute(query)
+        expect(response['errors']).not_to be_empty
       end
     end
   end
