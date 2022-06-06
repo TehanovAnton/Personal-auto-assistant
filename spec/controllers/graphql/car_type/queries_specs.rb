55404f2258bfc476d584081ed083a8dc30d9da19
anton <tehanovanton@gmail.com>
2022-06-06 17:49:50 +0300

    Car type queris tests

--- spec/controllers/graphql/car_type/queries_specs.rb
+++ spec/controllers/graphql/car_type/queries_specs.rb
@@ -31,4 +31,34 @@ RSpec.describe GraphqlController, type: :controller do
       expect(result['data']['cars']).not_to be_empty
     end
   end
+
+  describe '#car' do
+    let(:car) { create(:car, user: user) }
+    let(:query) do
+      <<~GQL
+        query {
+          car(id: #{car.id}) {
+            user {
+              firstName
+              lastName
+            }
+            model
+            yearProduction
+            engineVolume
+            mileage
+            bodyType
+            maker
+            vin
+            fuelType
+            transmissionType
+          }
+        }
+      GQL
+    end
+
+    it 'shoud return car' do
+      result = PersonalAutoAssitatntSchema.execute(query)
+      expect(result['data']['car']).not_to be_empty
+    end
+  end
 end
