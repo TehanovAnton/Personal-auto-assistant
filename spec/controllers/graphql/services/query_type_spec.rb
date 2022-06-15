9189a631cc5b26a39c8b4351754d1b236cfca7ca
anton <tehanovanton@gmail.com>
2022-06-15 15:45:29 +0300

    Service type specs

---
+++ spec/controllers/graphql/services/query_type_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GraphqlController, type: :controller do
+  let(:user) { organization.service_owner }
+  let!(:organization) { create(:organization_with_service_and_work, name: 'best company') }
+  let(:car) { create(:car_with_orders, user: user) } 
+  before { sign_in user }
+
+  describe '#services' do
+    context 'when query correct' do
+      include_examples "graphql query result shouldn't to be empty", 'services' do
+        let(:query) do
+          <<~GQL
+            query {
+              services(organizationId: #{organization.id}) {
+                name
+                address
+                phoneNumber
+                organization {
+                  name
+                }
+              }
+            }            
+          GQL
+        end
+      end 
+    end
+  end
+
+  describe '#service' do
+    context 'when query correct' do
+      include_examples "graphql query result shouldn't to be empty", 'service' do
+        let(:query) do
+          <<~GQL
+            query {
+              service(organizationId: #{organization.id}, serviceId: #{organization.services.last.id}) {
+                id
+                name
+                address
+                phoneNumber
+                organization {
+                  name
+                }
+              }
+            }            
+          GQL
+        end
+      end 
+    end
+  end
+end
--- spec/factories/services.rb
+++ spec/factories/services.rb
@@ -2,10 +2,17 @@
 
 FactoryBot.define do
   factory :service do
-    organization_id {}
-    name {}
+    organization_id { organization.id }
+    name { 'Best service' }
     email { Faker::Internet.safe_email }
     phone_number { Faker::PhoneNumber.cell_phone }
     address { "#{Faker::Address.city}, #{Faker::Address.street_name}, #{Faker::Address.building_number}" }
   end
+end
+
+def organization
+  organization = Organization.last
+  organization = create(:organization, name: 'Best organization') unless organization.present?
+  
+  organization
 end
