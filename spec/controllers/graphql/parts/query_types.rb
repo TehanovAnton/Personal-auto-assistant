759d456757c93490b0cab4379739888fb9989f69
anton <tehanovanton@gmail.com>
2022-06-15 15:48:26 +0300

    Parts type specs

---
+++ spec/controllers/graphql/parts/query_types.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GraphqlController, type: :controller do
+  let(:user) { create(:user, role: :car_owner) }
+  let(:car) { create(:car_with_orders, user: user) } 
+  before { sign_in user }
+
+  describe '#parts' do
+    context 'when query correct' do
+      include_examples "graphql query result shouldn't to be empty", 'parts' do
+        let(:query) do
+          <<~GQL
+            query {
+              parts(carId: #{car.id}) {
+                name
+              }
+            }            
+          GQL
+        end
+      end 
+    end
+  end
+
+  describe '#part' do
+    context 'when query correct' do
+      include_examples "graphql query result shouldn't to be empty", 'part' do
+        let(:query) do
+          <<~GQL
+            query {
+              part(carId: #{car.id}, partId: #{car.parts.first.id}) {
+                name
+              }
+            }            
+          GQL
+        end
+      end 
+    end
+  end
+end
