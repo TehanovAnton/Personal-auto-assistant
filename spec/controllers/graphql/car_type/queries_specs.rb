7e07a25c3dfff8e4d4bd256fcce81c751c81fa16
anton <tehanovanton@gmail.com>
2022-06-15 15:38:16 +0300

    Car type specs

--- spec/controllers/graphql/car_type/queries_specs.rb
+++ spec/controllers/graphql/car_type/queries_specs.rb
@@ -3,62 +3,63 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
+  let(:car) { create(:car, user: user) }
   let(:user) { create(:user, role: :car_owner) }
   before { sign_in user }
 
   describe '#cars' do
-    let(:query) do
-      <<~GQL
-        query {
-          cars {
-            userId
-            model
-            yearProduction
-            engineVolume
-            mileage
-            bodyType
-            maker
-            vin
-            fuelType
-            transmissionType
-          }
-        }
-      GQL
-    end
-
-    it 'should return array of cars' do
-      result = PersonalAutoAssitatntSchema.execute(query)
-      expect(result['data']['cars']).not_to be_empty
+    context 'car owner cars' do
+      include_examples "graphql query result should to be", 'cars' do
+        let(:query) do
+          <<~GQL
+            query {
+              cars {
+                user {
+                  firstName
+                  lastName
+                }
+                model
+                yearProduction
+                engineVolume
+                mileage
+                bodyType
+                maker
+                vin
+                fuelType
+                transmissionType
+              }
+            }
+          GQL
+        end
+      end
     end
   end
 
   describe '#car' do
-    let(:car) { create(:car, user: user) }
-    let(:query) do
-      <<~GQL
-        query {
-          car(id: #{car.id}) {
-            user {
-              firstName
-              lastName
+    context 'specified car' do
+      include_examples "graphql query result shouldn't to be empty", 'car' do
+        let(:query) do
+          <<~GQL
+            query {
+              car(id: #{car.id}) {
+                user {
+                  firstName
+                  lastName
+                }
+                model
+                yearProduction
+                engineVolume
+                mileage
+                bodyType
+                maker
+                vin
+                fuelType
+                transmissionType
+              }
             }
-            model
-            yearProduction
-            engineVolume
-            mileage
-            bodyType
-            maker
-            vin
-            fuelType
-            transmissionType
-          }
-        }
-      GQL
-    end
-
-    it 'shoud return car' do
-      result = PersonalAutoAssitatntSchema.execute(query)
-      expect(result['data']['car']).not_to be_empty
+          GQL
+        end
+      end
     end
   end
 end
