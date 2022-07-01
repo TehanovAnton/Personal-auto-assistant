c6095f5d91dcc46c748732af7edd8fe1b29c9f68
anton <tehanovanton@gmail.com>
2022-07-01 12:25:56 +0300

    User create mutation spec

--- spec/controllers/graphql/user_type/mutations_specs.rb
+++ spec/controllers/graphql/user_type/mutations_specs.rb
@@ -1,7 +1,43 @@
-require 'rails_hepler'
+require 'rails_helper'
 
-RSpec.describe GraphqlControler, type: :controller  do
+RSpec.describe GraphqlController, type: :controller  do
   describe '#create' do
-    
+    let(:userInput) do
+      {
+        userInput: {
+          userInput: {
+            firstName: "osavldo",
+            lastName: "lugones",
+            email: "lugones@gmail.com",
+            phoneNumber: "12345",
+            role: 0,
+            password: "ewqqwe"
+          }
+        }
+      }
+    end
+
+    let(:query) do
+      <<~GQL
+      mutation UserCreate($userInput: UserCreateInput!) {
+        userCreate(input: $userInput) {
+            user {
+              id
+              firstName
+              lastName
+              email
+              phoneNumber
+              role
+            }
+        }
+      }
+      GQL
+    end
+
+    it 'create user' do
+      binding.pry
+      result = PersonalAutoAssitatntSchema.execute(query, variables: userInput)
+      expect(result['data']['userCreate']).not_to be_empty
+    end
   end
 end
