326293b46ac9a2a2d869c2f9bb329e95fa112bf1
anton <tehanovanton@gmail.com>
2022-07-01 15:05:30 +0300

    Work creat update specs

---
+++ spec/controllers/graphql/works_type/mutations_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe GraphqlController, type: :controller do
+  let(:work) { create(:work) }
+  let(:category) { Category.create(name: 'drying') }
+
+  describe '#create' do
+    let(:mutation) do
+      <<~GQL
+      mutation WorkCreate($workInput: WorkCreateInput!) {
+        workCreate(input: $workInput) {
+          work {
+            id
+            title
+            categoryId
+          }
+        }
+      }
+      GQL
+    end
+
+    let(:variables) do
+      {
+        workInput: {
+          workInput: {
+            title: "drying",
+            categoryId: category.id
+          }
+        }
+      }
+    end
+
+    it 'create work' do
+      result = PersonalAutoAssitatntSchema.execute(mutation, variables: variables)
+      expect(result['data']['workCreate']).not_to be_empty
+    end
+  end
+
+  describe '#update' do    
+    let(:mutation) do
+      <<~GQL
+      mutation UpdateWork($workInput: WorkUpdateInput!) {
+        workUpdate(input:$workInput) {
+          work {
+            id
+            title
+            categoryId
+          }
+        }
+      }
+      GQL
+    end
+
+    let(:variables) do
+      {
+        workInput: {
+          id: work.id,
+          workInput: {
+            title: "updated title",
+            categoryId: category.id,
+          }
+        }
+      }
+    end
+
+    it 'update work' do
+      result = PersonalAutoAssitatntSchema.execute(mutation, variables: variables)
+      expect(result['data']['workUpdate']).not_to be_empty
+    end 
+  end
+end
