500b7db4aaaa17c50d944dfd83b4ce0bdf788d77
anton <tehanovanton@gmail.com>
2022-07-01 13:16:32 +0300

    Work create mutation

---
+++ app/graphql/mutations/works/work_create.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Works
+    class WorkCreate < BaseMutation
+      description "Creates a new work"
+  
+      field :work, Types::WorkType, null: false
+  
+      argument :work_input, Types::Inputs::WorkInputType, required: true
+  
+      def resolve(work_input:)
+        work = ::Work.new(**work_input)
+        raise GraphQL::ExecutionError.new "Error creating work", extensions: work.errors.to_hash unless work.save
+  
+        { work: work }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,8 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :work_create, mutation: Mutations::Works::WorkCreate
+
     field :user_delete, mutation: Mutations::Users::UserDelete
     field :user_create, mutation: Mutations::Users::UserCreate
     field :user_update, mutation: Mutations::Users::UserUpdate
