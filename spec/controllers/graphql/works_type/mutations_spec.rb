90c7bf8687958b7fe7f18d589c6ba7be00f71480
anton <tehanovanton@gmail.com>
2022-07-07 11:23:50 +0300

    Rework work mutations using shared examples

--- spec/controllers/graphql/works_type/mutations_spec.rb
+++ spec/controllers/graphql/works_type/mutations_spec.rb
@@ -7,96 +7,93 @@ RSpec.describe GraphqlController, type: :controller do
   let(:category) { Category.create(name: 'drying') }
 
   describe '#create' do
-    let(:mutation) do
-      <<~GQL
-        mutation WorkCreate($workInput: WorkCreateInput!) {
-          workCreate(input: $workInput) {
-            work {
-              id
-              title
-              categoryId
+    include_examples 'mutation return response' do
+      let(:mutation_resolver) { 'workCreate' }
+      let(:description) { 'create work' }
+      let(:mutation) do
+        <<~GQL
+          mutation WorkCreate($workInput: WorkCreateInput!) {
+            workCreate(input: $workInput) {
+              work {
+                id
+                title
+                categoryId
+              }
+            }
+          }
+        GQL
+      end  
+      let(:input_variables) do
+        {
+          workInput: {
+            workInput: {
+              title: 'drying',
+              categoryId: category.id
             }
           }
         }
-      GQL
-    end
-
-    let(:variables) do
-      {
-        workInput: {
-          workInput: {
-            title: 'drying',
-            categoryId: category.id
-          }
-        }
-      }
-    end
-
-    it 'create work' do
-      result = PersonalAutoAssitatntSchema.execute(mutation, variables: variables)
-      expect(result['data']['workCreate']).not_to be_empty
+      end
     end
   end
 
   describe '#update' do
-    let(:mutation) do
-      <<~GQL
-        mutation UpdateWork($workInput: WorkUpdateInput!) {
-          workUpdate(input:$workInput) {
-            work {
-              id
-              title
-              categoryId
+    include_examples 'mutation return response' do
+      let(:mutation_resolver) { 'workUpdate' }
+      let(:description) { 'update work' }
+      let(:work) { create(:work) }
+      let(:category) { Category.create(name: 'drying') }
+      let(:mutation) do
+        <<~GQL
+          mutation UpdateWork($workInput: WorkUpdateInput!) {
+            workUpdate(input:$workInput) {
+              work {
+                id
+                title
+                categoryId
+              }
+            }
+          }
+        GQL
+      end
+      let(:input_variables) do
+        {
+          workInput: {
+            id: work.id,
+            workInput: {
+              title: 'updated title',
+              categoryId: category.id
             }
           }
         }
-      GQL
-    end
-
-    let(:variables) do
-      {
-        workInput: {
-          id: work.id,
-          workInput: {
-            title: 'updated title',
-            categoryId: category.id
-          }
-        }
-      }
-    end
-
-    it 'update work' do
-      result = PersonalAutoAssitatntSchema.execute(mutation, variables: variables)
-      expect(result['data']['workUpdate']).not_to be_empty
+      end
     end
   end
 
   describe '#delete' do
-    let(:mutation) do
-      <<~GQL
-        mutation WorkDelete($workInput:WorkDeleteInput!) {
-          workDelete(input: $workInput) {
-            work {
-              id
-              title
-              categoryId
+    include_examples 'mutation return response' do
+      let(:mutation_resolver) { 'workDelete' }
+      let(:description) { 'delete work' }
+      let(:work) { create(:work) }
+      let(:mutation) do
+        <<~GQL
+          mutation WorkDelete($workInput:WorkDeleteInput!) {
+            workDelete(input: $workInput) {
+              work {
+                id
+                title
+                categoryId
+              }
             }
           }
+        GQL
+      end
+      let(:input_variables) do
+        {
+          "workInput": {
+            "id": work.id
+          }
         }
-      GQL
-    end
-
-    let(:variables) do
-      {
-        "workInput": {
-          "id": work.id
-        }
-      }
-    end
-
-    it 'delete work' do
-      result = PersonalAutoAssitatntSchema.execute(mutation, variables: variables)
-      expect(result['data']['workDelete']).not_to be_empty
-    end
+      end
+    end 
   end
 end
